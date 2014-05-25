5bacaf189a547677bd7acffa9d96117eec1779b1
medhini <medhini95@gmail.com>
2014-05-25 20:00:39 +0530

    Changed some urls

--- Profile/urls.py
+++ Profile/urls.py
@@ -5,7 +5,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Profile.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    #url(r'^blog/', include('blog.urls')),
+    url(r'^/$', 'Profile.views.login'),
     url(r'^accounts/login/$', 'Profile.views.login'),
     url(r'^accounts/auth/$', 'Profile.views.auth_view'),
     url(r'^accounts/logout/$', 'Profile.views.logout'),
